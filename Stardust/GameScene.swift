77bd114737c38b014e47eca32bb9c1e545bc7c95
Олеся Мартынюк <olesyamartinyuk@yandex.ru>
2020-04-03 23:55:12 +0300

    collisions added

--- Stardust/GameScene.swift
+++ Stardust/GameScene.swift
@@ -9,7 +9,7 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     let girl = SKSpriteNode(imageNamed: "GG1")
     var lastUpdateTime: TimeInterval = 0
     var deltaTime: TimeInterval = 0
@@ -22,6 +22,9 @@ class GameScene: SKScene {
     let girlAnimation: SKAction
     let asteroidAnimation: SKAction
     let starAnimation: SKAction
+    let girlCategory:UInt32 = 0x1 << 1;
+    let asteroidCategory:UInt32 = 0x1 << 2;
+    let starCategory:UInt32 = 0x1 << 3;
     
     override init(size: CGSize) {
         let maxApectRatio: CGFloat = deviceHeight / deviceWidth
@@ -30,6 +33,7 @@ class GameScene: SKScene {
         let playableMargin = (size.width - playableWidth) / 2.0
         // пря-ник с максимальным соотношением сторон, в центре которого находится экран
         playableRect = CGRect(x: 0, y: playableMargin, width: playableWidth, height: size.height)
+        
         var texturesGirl:[SKTexture] = []
         for i in 1...6 {
             texturesGirl.append(SKTexture(imageNamed: "GG\(i)"))
@@ -52,6 +56,9 @@ class GameScene: SKScene {
         fatalError("init(coder:) has not been implemented")
     }
     override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        physicsWorld.contactDelegate = self
+        
         backgroundColor = SKColor.black // начальные настройки сцены
         let background = SKSpriteNode(imageNamed: "backgroundSpace")
         background.anchorPoint = CGPoint(x: 0, y: 0)
@@ -59,8 +66,16 @@ class GameScene: SKScene {
         background.size = CGSize(width: background.size.width / (background.size.height / deviceHeight), height: deviceHeight)
         background.zPosition = -1
         addChild(background)
+        
         girl.position = CGPoint(x: background.size.width / 2, y: 250)
+        girl.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: girl.size.width - 50, height: girl.size.height - 50))
+        girl.physicsBody!.contactTestBitMask = starCategory + asteroidCategory
+        girl.name = "girl"
+        girl.physicsBody?.categoryBitMask = girlCategory
+        girl.physicsBody?.isDynamic = false
+        
         addChild(girl)
+        
         run(SKAction.repeatForever(SKAction.sequence([SKAction.run({
             [weak self] in self?.generateAsteroid()
         }),
@@ -168,9 +183,14 @@ class GameScene: SKScene {
     }
     
     func generateAsteroid() {
-        let timeIntervalAsteroid: TimeInterval = TimeInterval(Float.random(in: 1 ... 3))
+        let timeIntervalAsteroid: TimeInterval = TimeInterval(Float.random(in: 2 ... 3))
         let asteroid = SKSpriteNode(imageNamed: "Ast1")
         asteroid.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        asteroid.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: asteroid.size.width - 50, height: asteroid.size.height - 50))
+        asteroid.name = "asteroid"
+        asteroid.physicsBody!.contactTestBitMask = girlCategory
+        asteroid.physicsBody?.categoryBitMask = asteroidCategory
+        
         let positionX = Int.random(in: 0 ... Int(size.width))
         asteroid.position = CGPoint(x: positionX, y: Int(size.height) + Int(asteroid.size.height) / 2)
         addChild(asteroid)
@@ -178,6 +198,7 @@ class GameScene: SKScene {
         let actionDownRight = SKAction.move(to: CGPoint(x: Int(size.width)/2 + Int(size.width)/4, y: 0 - Int(asteroid.size.height) / 2), duration: timeIntervalAsteroid)
         let actionDownLeft = SKAction.move(to: CGPoint(x: Int(size.width)/2 - Int(size.width)/4, y: 0 - Int(asteroid.size.height) / 2), duration: timeIntervalAsteroid)
         let actionDelete = SKAction.removeFromParent()
+        
         let randomMove = Int.random(in: 0 ..< 3)
         if randomMove == 0 {
             startAnimation(sprite: asteroid, spriteAction: asteroidAnimation)
@@ -194,9 +215,14 @@ class GameScene: SKScene {
     }
     
     func generateStars() {
-        let timeIntervalStar: TimeInterval = TimeInterval(Float.random(in: 1.5...5))
+        let timeIntervalStar: TimeInterval = TimeInterval(Float.random(in: 3...5))
         let star = SKSpriteNode(imageNamed: "Star1")
+        star.physicsBody = SKPhysicsBody(rectangleOf: star.size)
+        star.name = "star"
+        star.physicsBody!.categoryBitMask = starCategory
+        star.physicsBody!.contactTestBitMask = girlCategory
         star.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
         let positionX = Int.random(in: 0 ... Int(size.width))
         star.position = CGPoint(x: positionX, y: Int(size.height) + Int(star.size.height) / 2)
         addChild(star)
@@ -217,5 +243,29 @@ class GameScene: SKScene {
     
     func stopAnimation(sprite: SKSpriteNode, spriteAction:SKAction) {
         sprite.removeAction(forKey: "animation")
+    }
+    
+    func collision(between girl: SKNode, object: SKNode) {
+        if object.name == "star" {
+            destroy(object: object)
+        }
+        if object.name == "asteroid" {
+            destroy(object: object)
+        }
+    }
+    
+    func destroy(object: SKNode) {
+        object.removeFromParent()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        guard let nodeA = contact.bodyA.node else {return}
+        guard let nodeB = contact.bodyB.node else {return}
+        if contact.bodyA.node?.name == "girl" {
+            collision(between: nodeA, object: nodeB)
+        }
+        if contact.bodyB.node?.name == "girl" {
+            collision(between: nodeB, object: nodeA)
+        }
     }
 }
